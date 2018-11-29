df6f47a5ab717f86d176f67d55e4de7050cbc261
wang935415150 <wb-wyd358756@alibaba-inc.com>
2018-11-29 19:20:00 +0800

    学习,react的第一步创建一个井字游戏

--- src/index.js
+++ src/index.js
@@ -10,7 +10,25 @@ class Square extends React.Component {
     );
   }
 }
-
+function calculateWinner(squares){
+  let lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0 ; i<lines.length;i++){
+    let [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+};
 class Board extends React.Component {
   constructor(props){
   	super(props);
@@ -21,6 +39,9 @@ class Board extends React.Component {
   }
   handleClick(i){
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? "x":"O";
     this.setState({squares:squares,
                     xIsNext:!this.state.xIsNext,
@@ -34,10 +55,14 @@ class Board extends React.Component {
     	/>
   ) ;
 }
-
-
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? "X":"o");
+    const winner = calculateWinner(this.state.squares)
+    let status ;
+    if (winner){
+      status = "winner" + winner
+    }else{
+    status = "Next player: " + (this.state.xIsNext ? "X":"O")};
+  
     return (
       <div>
         <div className="status">{status}</div>
